43d9577a309b6186693055509ec32a64395370f0
unknown <jose.cancino@tecsup.edu.pe>
2021-04-18 17:24:48 -0500

    Informacion del Curso Primer punto

---
+++ componentes/cursos.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+
+function Cursos (props) {
+
+        
+   const cursos = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          }
+        ]
+      }
+
+      const lista = cursos.parts   
+
+    return(
+    <div className ="container">
+        <h1>Lista de los cursos</h1>
+        {
+            lista.map(lis =>
+                <div key={lis.id}>          
+                    <p className="nombre">Nombre del Curso {lis.id} : </p>
+                    <p> {lis.name}</p>
+                    <p>Ejercicio: {lis.exercises}</p>
+                    <p>-----------------------------------------</p>
+                </div>
+                )
+        }
+    </div>
+    )
+}
+
+
+export default Cursos
