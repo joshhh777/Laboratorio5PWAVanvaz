1e0cb27c194ab1df957470ed32263606921e9414
unknown <jose.cancino@tecsup.edu.pe>
2021-04-18 18:18:57 -0500

    variedad de cursos punto 3

--- componentes/cursos.js
+++ componentes/cursos.js
@@ -5,47 +5,95 @@ import Total from "./total"
 function Cursos (props) {
 
         
-   const cursos = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          }
-        ]
+    const cursos = [
+        {
+          name: 'Half Stack application development',
+          id: 1,
+          parts: [
+            {
+              name: 'Fundamentals of React',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Using props to pass data',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'State of a component',
+              exercises: 14,
+              id: 3
+            },
+            {
+              name: 'Redux',
+              exercises: 11,
+              id: 4
+            }
+          ]
+        }, 
+        {
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewares',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
+      ]
+
+        
+
+
+      function listado1(){
+        
+        const lista1 = cursos[0].parts  
+        return(
+        lista1.map(lista =>
+            <div key={lista.id}>          
+                <p className="nombre">Nombre del Curso {lista.id} : </p>
+                <p> {lista.name}</p>
+                <p>Ejercicio: {lista.exercises}</p>
+                <p>-----------------------------------------</p>
+                <Total parte={lista1}></Total>
+            </div>)         
+        )
       }
 
-      const lista = cursos.parts   
+      function listado2(){
+         
+        const lista2 = cursos[1].parts 
+        return(
+        lista2.map(lista =>
+            <div key={lista.id}>          
+                <p className="nombre">Nombre del Curso {lista.id} : </p>
+                <p> {lista.name}</p>
+                <p>Ejercicio: {lista.exercises}</p>
+                <p>-----------------------------------------</p>
+                <Total parte={lista2}></Total>
+            </div>)        
+        )
+      }
 
     return(
     <div className ="container">
         <h1>Lista de los cursos</h1>
-        {
-            lista.map(lis =>
-                <div key={lis.id}>          
-                    <p className="nombre">Nombre del Curso {lis.id} : </p>
-                    <p> {lis.name}</p>
-                    <p>Ejercicio: {lis.exercises}</p>
-                    <p>-----------------------------------------</p>
-                </div>
-                )
-        }
-        <Total parte={lista}></Total>
+        <h2>{cursos[0].name}</h2>
+        {listado1()}
+        
+        <p>###############################################</p>
+        <h2>{cursos[1].name}</h2>
+        {listado2()}
     </div>
     )
 }
-
 
 export default Cursos
